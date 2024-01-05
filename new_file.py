3fe6ab172eac752283294a8fb087c018446fb71b
Michael Maggs <runawayserver@infinitelyloopee.dev>
2024-01-05 13:23:47 -0800

    cleaning up

--- new_file.py
+++ new_file.py
@@ -1,4 +1,4 @@
 # testing which bracnch is being edited when I switch branched in the cli 
 var1 = 'this is the line that will be kept'
 var2 = 'this is the line that will be deleted'
-var3 = 'this is the line that will be edited'
+var3 = 'this is the line that will be edited'git
